f370d5b300917282b655ea47d036d58628082b89
Thomas Fruehwirth <e0927088@student.tuwien.ac.at>
2013-12-24 23:03:55 +0100

    echten workspace fuer den java teil eingecheckt. unter windows getestet, ka ob der auch unter linux funktioniert. sollte aber

---
+++ Java_Workspace/HBA/src/KNXDatapoint.java
@@ -0,0 +1,30 @@
+import tuwien.auto.calimero.datapoint.Datapoint;
+import tuwien.auto.calimero.process.ProcessCommunicator;
+
+
+public abstract class KNXDatapoint extends Thread{
+	protected String value;
+	public String name;
+	ProcessCommunicator pc;
+	RemoteCommunicator rc;
+	Datapoint dp;
+	protected String debugGAString;
+	
+	protected String getDPT(String dpt)
+	{
+		if (dpt.equals("switch"))
+			return "1.001";
+		if (dpt.equals("bool"))
+			return "1.002";
+		if (dpt.equals("string"))
+			return "16.001";
+		if (dpt.equals("float"))
+			return "9.002";
+		if (dpt.equals("ucount"))
+			return "5.010";
+		if (dpt.equals("angle"))
+			return "5.003";
+		return dpt;
+	}
+
+}
